5b543e96ca183aa3f4339d123ee971a5baada351
Juan Camilo Aranda <camiloaranda07@hotmail.com>
2025-11-19 17:59:14 -0500

    endpoint movies y movies controller

--- src/main/java/com/platzi/play/persistence/crud/CrudMovieEntity.java
+++ src/main/java/com/platzi/play/persistence/crud/CrudMovieEntity.java
@@ -3,7 +3,7 @@ package com.platzi.play.persistence.crud;
 import com.platzi.play.persistence.entity.MovieEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CurdMovieEntity extends CrudRepository<MovieEntity, Long> {
+public interface CrudMovieEntity extends CrudRepository<MovieEntity, Long> {
 
 
 }
