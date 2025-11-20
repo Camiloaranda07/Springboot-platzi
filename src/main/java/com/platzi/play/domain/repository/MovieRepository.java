9019335e730cdbcfd4c2188822730103e44ea35c
Juan Camilo Aranda <camiloaranda07@hotmail.com>
2025-11-20 17:38:11 -0500

    implementacion arquitectura orientada al dominio

---
+++ src/main/java/com/platzi/play/domain/repository/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.platzi.play.domain.repository;
+
+import com.platzi.play.domain.dto.MovieDto;
+import com.platzi.play.persistence.entity.MovieEntity;
+
+import java.util.List;
+
+public interface MovieRepository {
+    List<MovieDto> getAll();
+}
